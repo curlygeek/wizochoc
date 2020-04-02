bae716cb795b5ff8db20e31d890eefad92d1d045
josephine <4753015+curlygeek@users.noreply.github.com>
2020-04-02 14:15:54 +1300

    Basic reuirements to submit to packagist and test

---
+++ src/Extensions/SiteConfigExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Honeydukes\Wizochoc\Extensions;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\ORM\DataExtension;
+
+class SiteConfigExtension extends DataExtension 
+{
+    
+    private static $db = [
+        'GTMContainerID' => 'Varchar'
+    ];
+
+    public function updateCMSFields(FieldList $fields) 
+    {
+        $fields->addFieldToTab("Root.Main", new HTMLEditorField("GTMContainerID", "Google Tag Manager container ID")
+        )->description('Your GTM container ID will look like this GTM-XXXXXXX.');
+    }
+}
---
+++ src/Extensions/SocialPageExtension.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Honeydukes\Wizochoc\Extensions;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Assets\Image;
+use SilverStripe\ORM\FieldType\DBEnum;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\DropdownField;
+
+class SocialPageExtension extends DataExtension
+{
+
+    private static $db = [
+        "SocialMediaTitle" => "Text",
+        "SocialMediaDescription" => "Text",
+        "TwitterCardType" => 'Enum("Summary, Summary with large image, App, Player")'
+    ];
+    
+/** FOR REFERENCE WHEN DOING TEMPLATE
+    <meta property="og:title" content="European Travel Destinations">
+    <meta property="og:description" content="Offering tour packages for individuals or groups.">
+    <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg">
+    <meta property="og:url" content="http://euro-travel-example.com/index.htm">
+    <meta name="twitter:card" content="summary_large_image"> //The card type, which will be one of “summary”, “summary_large_image”, “app”, or “player”.
+**/
+    
+    private static $has_one = [
+        "SocialMediaImage" => Image::class
+    ];    
+ 
+    public function updateCMSFields(FieldList $fields) 
+    {
+        
+        $SocialMediaTitle= TextField::create(
+            "SocialMediaTitle", "Social media title" , $this->owner->SocialMediaTitle
+        )->setDescription('Set the title to show in social media posts. If left blank, the page name will be used');
+
+        
+        $SocialMediaDescription= TextField::create(
+            "SocialMediaDescription", "Social media description" , $this->owner->SocialMediaDescription
+        )->setDescription('Set the description to show in social media posts. If left blank, the meta description will be used');
+
+        $SocialMediaImage = UploadField::create(
+            "SocialMediaImage",
+            "Social media thumbnail"
+        );
+        
+        $SocialMediaImage->setAllowedMaxFileNumber(1);
+        $SocialMediaImage->setAllowedFileCategories("image");
+        $SocialMediaImage->setFolderName("/SocialMediaThumbnails");
+        $SocialMediaImage->setDescription("An ideal image should be approximately 1200 pixels wide x 630 pixels high. Optimise the image to keep the size under 1MB.");
+        
+        $TwitterCardType = DropdownField::create(
+            'TwitterCardType',
+            'Twitter card type',
+            $this->owner->dbObject('TwitterCardType')->enumValues()
+        );
+       $TwitterCardType->setDescription("The Twitter card type to use in posts");
+ 
+        
+        $fields->addFieldsToTab('Root.SocialMedia', [
+            $SocialMediaTitle,
+            $SocialMediaDescription,
+            $SocialMediaImage,
+            $TwitterCardType
+        ]);
+    }
+    
+}
