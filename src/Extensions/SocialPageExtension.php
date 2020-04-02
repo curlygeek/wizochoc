8701f6648d6755a4f289dc9d2d6adf8d927f42d9
josephine <4753015+curlygeek@users.noreply.github.com>
2020-04-02 21:52:50 +1300

    Please let this work now

--- src/Extensions/SocialPageExtension.php
+++ src/Extensions/SocialPageExtension.php
@@ -62,5 +62,22 @@ class SocialPageExtension extends DataExtension
         ]);
     }
     
+    public function socialMetadata()
+    {
+        $metadata = '';
+        
+        if ($this->owner->SocialMediaTitle != '') {
+            $metadata += '<meta property="og:title" content="'. $this->owner->SocialMediaTitle . '">';
+        }
+    
+/**<meta property="og:description" content="Offering tour packages for individuals or groups.">
+    <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg">
+    <meta property="og:url" content="http://euro-travel-example.com/index.htm">
+    <meta name="twitter:card" content="summary_large_image"> //The card type, which will be one of “summary”, “summary_large_image”, “app”, or “player”.
+   
+**/
+        return $metadata;
+    }
+    
     
 }
